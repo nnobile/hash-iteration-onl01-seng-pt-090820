6789d50dad447de37256943530dbcfd793438714
Nick Nobile <nickcnobile@gmail.com>
2020-09-20 01:00:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,10 +4,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_kids)
-birthday_kids.collect do |kids_name, age|
-  if age < 12
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
+def happy_birthday(birthday_kids, age_appropriate = false)
+  birthday_kids.each do |kid,age|
+    if (age < 13 || !age_appropriate) 
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    else
+      puts "#{kid} you are too old for this."
+    end 
+  end 
 end
