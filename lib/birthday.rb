bab73e19b6609ab0245081760d0611aea02546b7
Nick Nobile <nickcnobile@gmail.com>
2020-09-20 00:56:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,7 +1,7 @@
 birthday_kids = { "Timmy" => 9, "Sarah" => 6, "Amanda" => 27}
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kid, age|
-    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
